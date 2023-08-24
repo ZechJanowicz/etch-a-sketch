bc492ec6db7fa9c99f08aba774418b79bf3a1d86
Zechariah Janowicz <zechariahjanowicz@gmail.com>
2023-08-24 12:44:30 -0500

    added javascript to prompt the user for the grid size, will end up trying to make the userinterface to look better.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,31 @@
 console.log("Hello world")
+
+function addDiv() {
+var div = document.createElement('div');
+div.style.flexGrow = "1";
+div.style.background = "black";
+div.style.borderWidth = "1px";
+div.style.borderColor = "white";
+div.style.borderStyle = "solid";
+div.innerHTML = "&nbsp";
+
+return document.getElementById('grid').appendChild(div);
+}
+
+
+/*64 for now*/
+
+
+const canvasSpace = () => {
+    userInput = prompt("Enter amount: ")
+    return userInput;
+}
+
+function gridMod() {
+    canvasSpace();
+    for (i = 0; i < userInput; i++) {
+        addDiv();
+    }
+}
+
+gridMod();
