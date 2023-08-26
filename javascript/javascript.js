afb9248a641a0f8a33767e52f6033b5c0c99cd4c
Zechariah Janowicz <zechariahjanowicz@gmail.com>
2023-08-26 12:44:19 -0500

    implemented javascript using a foreach function to tackle each item within the class one at a time to then add an eventlistener that will take the color from a function and pass it to the function that will end up changing its color.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,21 +1,31 @@
 console.log("Hello world")
 amountSpace = ""
 
-function addDiv() {
-var div = document.createElement('div');
-div.style.background = "black";
-div.style.borderWidth = "1px";
-div.style.borderColor = "white";
-div.style.borderStyle = "solid";
+var addDiv = () => {
 
-div.innerHTML = "&nbsp";
+newDiv = document.createElement('div');
+newDiv.style.borderWidth = "1px";
+newDiv.style.borderColor = "white";
+newDiv.style.borderStyle = "solid";
+newDiv.innerHTML = "&nbsp";
 
-return document.getElementById('grid').appendChild(div);
+newDiv.className = "divClass"
+return document.getElementById('grid').appendChild(newDiv) + newDiv;
 }
 
+function colorSquare() {
+    this.style.backgroundColor = "pink";
+}
 
 /*64 for now*/
+/*Make a selector for a color*/
 
+function placeEventListenerOnTiles() {
+    let allTiles = document.querySelectorAll(".divClass");
+    allTiles.forEach(element => {
+        element.addEventListener("mouseover", colorSquare);
+    })
+}
 
 const canvasSpace = () => {
     userInput = prompt("Amount: 1 to 64: ")
@@ -23,9 +33,15 @@ const canvasSpace = () => {
     return sqrtInput + userInput;
 }
 
+
+
 function gridSpace() {
     document.getElementById('grid').style.gridTemplateColumns = `repeat(${userInput}, 1fr)`
+    document.getElementById('grid').style.gridTemplateRows = `repeat(${userInput}, 1fr)`
+
 }
+
+
 
 function gridMod() {
     canvasSpace();
@@ -35,5 +51,7 @@ function gridMod() {
     }
 }
 
-gridMod();
 
+
+gridMod();
+placeEventListenerOnTiles();
